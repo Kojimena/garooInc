78d17b72ea985f646c5f877ae9abee4d178e9edf
Jimena Hernández <77686378+Kojimena@users.noreply.github.com>
2023-06-15 14:00:17 -0600

    navbar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -13,9 +13,9 @@ const Navbar = () => {
     return (
         <div className='header'>
             <nav className='navbar'>
-                <a href='/' className='logo'>
+                <Link to='/' className='logo'>
                     <img src="/images/logo.png" alt='logo' />
-                </a>
+                </Link>
                 <div className='hamburger' onClick={handleClick}>
                     {click ? (<FaTimes size={30} style={{ color: "#ffffff" }} />)
                         : (<FaBars size={30} style={{ color: "#ffffff" }} />)}
@@ -23,19 +23,19 @@ const Navbar = () => {
                 </div>
                 <ul className={click ? "nav-menu active" : "nav-menu"}>
                     <li className='nav-item'>
-                        <Link to="/" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>Home</Link>
+                        <Link to="/" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Home</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to="about" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>About</Link>
+                        <Link to="about" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>About</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to="services" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>Services</Link>
+                        <Link to="services" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Services</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to="portfolio" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>Portfolio</Link>
+                        <Link to="portfolio" spy={true} smooth={true} offset={-100} duration={500} onClick={closeMenu}>Portfolio</Link>
                     </li>
                     <li className='nav-item'>
-                        <Link to="contact" spy={true} smooth={true} offset={50} duration={500} onClick={closeMenu}>Contact Us</Link>
+                        <Link to="contact" spy={true} smooth={true} offset={-150} duration={500} onClick={closeMenu}>Contact Us</Link>
                     </li>
                 </ul>
             </nav>
